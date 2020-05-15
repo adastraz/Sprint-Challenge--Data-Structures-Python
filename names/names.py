a0eaee0199939a1cde8f3db98d38e39ba3d8e718
adastraz <tylergosox@gmail.com>
2020-05-15 12:42:03 -0400

    wrong directory, added names here

--- names/names.py
+++ names/names.py
@@ -1,10 +1,16 @@
 import time
+from binary_search_tree import BSTNode
 
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
+
+with open('names_1.txt', 'r') as x:
+    names = BSTNode(x.readline())
+    for name in x.readlines():
+        names.insert(name.strip())
 
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
@@ -13,10 +19,9 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+for name in names_2:
+    if names.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
