03b211ab13d33816b00543a713d20544dfc93278
adastraz <tylergosox@gmail.com>
2020-05-15 11:03:57 -0400

    tests are messed up, but ring is working!

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,14 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.stuff = []
 
     def append(self, item):
-        pass
+        if len(self.stuff) < self.capacity:
+            self.stuff.append(item)
+        else:
+            self.stuff.remove(self.stuff[0])
+            self.stuff.append(item)
 
     def get(self):
-        pass
+        return(self.stuff)
