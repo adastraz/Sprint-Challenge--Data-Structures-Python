4a42d52fa6c90e80414182f14b876b42452e46dd
adastraz <tylergosox@gmail.com>
2020-05-15 13:41:20 -0400

    :/ not what I wanted, but it works

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -1,16 +1,21 @@
 class Node:
-    def __init__(self, value=None, next_node=None):
+    def __init__(self, value=None, next_node=None, prev=None):
         self.value = value
         self.next_node = next_node
+        self.prev = prev
 
     def get_value(self):
         return self.value
 
     def get_next(self):
+        # return self.prev
         return self.next_node
 
     def set_next(self, new_next):
         self.next_node = new_next
+
+    def set_prev(self, prev):
+        self.prev = prev
 
 class LinkedList:
     def __init__(self):
@@ -21,6 +26,7 @@ class LinkedList:
 
         if self.head is not None:
             node.set_next(self.head)
+            self.head.set_prev(node)
 
         self.head = node
 
@@ -39,4 +45,22 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        # if not node.next: 
+        #     self.head = node
+        #     node.set_next('end')
+        #     print(node)
+        #     self.reverse_list(node.prev)
+        # if node.prev:
+        #     print(node)
+        #     self.reverse_list(node.prev)
+        # else:
+        #     print(node)
+        head = self.head
+        prev = None
+        while head is not None:
+            next_node = head.next_node
+            head.next_node = prev
+            prev = head
+            head = next_node
+        self.head = prev
+
