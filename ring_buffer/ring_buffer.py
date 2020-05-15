7978f3a2e50072785b4defd65da02bee2918b172
adastraz <tylergosox@gmail.com>
2020-05-15 12:41:07 -0400

    completed names, and fixed error with ringbuffer! not a testing issue!

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -2,13 +2,18 @@ class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.stuff = []
+        self.oldest = 0
 
     def append(self, item):
         if len(self.stuff) < self.capacity:
             self.stuff.append(item)
         else:
-            self.stuff.remove(self.stuff[0])
-            self.stuff.append(item)
+            self.stuff.remove(self.stuff[self.oldest])
+            self.stuff.insert(self.oldest, item)
+            if self.oldest+1 < self.capacity:
+                self.oldest += 1
+            else:
+                self.oldest = 0
 
     def get(self):
         return(self.stuff)
